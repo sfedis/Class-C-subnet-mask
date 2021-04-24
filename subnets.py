ba6e9dbe89c1d101327bd94bdd834d97cfad32a9
sfedis <sfendourakispan@gmail.com>
2021-04-24 09:16:47 +0300

    Refactored code

--- subnets.py
+++ subnets.py
@@ -1,60 +1,56 @@
-def single_mask(user_input):
+def calcMask(user_input):
+    subnet_bits = user_input-24
+    subnets = 2**(subnet_bits)
+    host_bits = (32-user_input)
+    dist = 2**(host_bits)
+    subnet_address = list()
+    broadcast_address = list()
 
-    dist = 2**(32-user_input)
-    print(f"subnets: {2**(user_input-24)}")
-    print(f"hosts/subnet: {dist-2}")
-    print("subnet address (last byte):", end=" ")
     i=0
-    while (i<255):
-        print(f"{i}", end=" ")
+    while (i<256):
+        subnet_address.append(i)
+        broadcast_address.append(i+dist-1)
         i += dist
-    print("")
-    
-    print("broadcast address (last byte):", end=" ")
-    j=0
-    while (j<256):
-        print(f"{j+dist-1}", end=" ")
-        j += dist
-    print("")
-    
-def whole_mask(user_input):
-    zeros = 0
-    j=0
-    lastByte = int(user_input.split(".")[-1])
-    for i in range(7, 0, -1):
-        j += 2**i
-        if lastByte == j:
-            zeros = i
-    if zeros != 0:
-        single_mask(32-zeros) 
-    else:
-        print("Not a valid argument")
+    print(f"subnets: {subnets}")
+    print(f"hosts/subnet: {2**host_bits-2}")
+    print("subnet address (last byte): " + " ".join(map(str, subnet_address)))
+    print("broadcast_address (last byte): " + " ".join(map(str, broadcast_address)))
     return
 
+
 def isMask(user_input):
+
+    if (len(user_input) == 2 and int(user_input) <= 30 and int(user_input) >= 25):
+        return calcMask(int(user_input))
+
     try:
         lastByte = int(user_input.split(".")[-1])
         if (lastByte >=0 and lastByte <= 255):
             for i in user_input.split(".", 2):
                 if (i == '255'):
                     continue
-            return True
     except:
+        print("Not a valid argument")
         return False
+
+    zeros = 0
+    j=0
+    lastByte = int(user_input.split(".")[-1])
+    try:
+        for i in range(7, 0, -1):
+            j += 2**i
+            if lastByte == j:
+                zeros = i
+        if zeros != 0:
+            return calcMask(32-zeros)
+    except:
+        print("Not a valid argument")
+        return False
+
 
 def main():
     user_input = input("Type your C class subnet mask:")
-
-    if (len(str(user_input)) == 2 and int(user_input) <= 30 and int(user_input) >= 25):
-        single_mask(int(user_input))
-        return
-    
-    #check if input is valid mask
-    if (isMask(user_input)):
-        whole_mask(user_input)
-        return
-    else:
-        print("Not a valid argument")
+    isMask(user_input)
 
 
 if __name__ == "__main__" :
